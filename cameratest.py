1834ae54f7774c44e6c9f2742a0d299dab1e57ae
ryukey_haya <20195117@g.aut.ac.jp>
2021-12-03 15:56:31 +0900

    add cameratest

--- cameratest.py
+++ cameratest.py
@@ -1,9 +1,25 @@
 import cv2
+import time
 
-cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+fps=0
+fpsc=0
+fpstime=time.time()
 
 while True:
+    fpsc=fpsc+1
+    if(time.time()-fpstime>1):
+        fps=fpsc
+        fpsc=0
+        fpstime=time.time()
+
     ret, frame = cap.read()
+    cv2.putText(frame,"FPS:{0}".format(fps),(10,200),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),2,cv2.LINE_AA)
+
     cv2.imshow('demo', frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
